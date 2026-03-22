e07dab2bf50c1d5b6eacfa6ede68b2540b7d08a1
gous123shaikh <shaikhmg29@gmail.com>
2026-03-22 17:08:13 +0530

    Rectangle area feature completed

---
+++ geometry.py
@@ -0,0 +1,23 @@
+import math
+
+class GeometryCalculator:
+
+    def calculate_circle_area(self, radius):
+        return math.pi * radius ** 2
+
+    def calculate_rectangle_area(self, length, width):
+        return length * width
+
+
+calculator = GeometryCalculator()
+
+radius = 5
+print(calculator.calculate_circle_area(radius))
+
+calculator = GeometryCalculator()
+
+length = 10
+width = 6
+print(calculator.calculate_rectangle_area(length, width))
+
+
